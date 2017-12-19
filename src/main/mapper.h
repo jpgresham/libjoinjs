e7697bec734170acb438d57d005acab8e06545fb
jpgresham <jpgresham@gmail.com>
2017-12-19 17:39:11 -0500

    Adding changes made to mapping schema to JsonSchema struct

--- src/main/mapper.h
+++ src/main/mapper.h
@@ -11,7 +11,7 @@
 #include <rapidjson/reader.h>
 #include <set>
 #include <unordered_map>
-
+#include <spdlog/spdlog.h>
 
 using namespace std;
 using namespace rapidjson;
@@ -40,15 +40,20 @@ namespace jjn {
 
     class SchemaJsonHandler  : public BaseReaderHandler<UTF8<>, SchemaJsonHandler> {
     private:
-        unordered_map<string, JsonSchema> *jsonSchemaSetPtr;
+        unordered_map<string, JsonSchema> jsonSchemaSetPtr;
+        shared_ptr<spdlog::logger> console = spdlog::stdout_color_mt("mapper");
         string lastKey;
+        string currentObjectKey;
         int objectDepth = 0;
         int arrayDepth = 0;
+        bool insidePropertiesArr = false;
+        bool insideAssociationsObj = false;
+        bool insideCollectionsObj = false;
     public:
 
         SchemaJsonHandler() {
             std::cout << "In the Constructor" << std::endl;
-            jsonSchemaSetPtr = new unordered_map<string, JsonSchema>();
+            jsonSchemaSetPtr = unordered_map<string, JsonSchema>();
         }
 
         bool Null() { std::cout << "Null()" << std::endl; return true; }
@@ -66,11 +71,13 @@ namespace jjn {
             if (lastKey.empty()) {
                 cout << "Setting up the JSON schema" << endl;
             } else {
-                auto find = jsonSchemaSetPtr->find(lastKey);
-                if (find == jsonSchemaSetPtr->end()) {
-                    jsonSchemaSetPtr->insert(make_pair(lastKey, *new JsonSchema()));
-                    //this->jsonSchemaSetPtr->mapId = (char *) malloc(length);
-                    //this->jsonSchemaSetPtr->mapId = const_cast<char *>(str);
+                auto find = jsonSchemaSetPtr.find(currentObjectKey);
+                if (find != jsonSchemaSetPtr.end()) {
+                    JsonSchema *jsonSchema = &jsonSchemaSetPtr.at(currentObjectKey);
+                    if (strcmp(lastKey.c_str(), "mapId") == 0) {
+                        jsonSchema->mapId = (char *) malloc(length);
+                        jsonSchema->mapId = const_cast<char *>(str);
+                    }
                 }
             }
             std::cout << "String(" << str << ", " << length << ", " << std::boolalpha << copy << ")" << std::endl;
@@ -78,21 +85,45 @@ namespace jjn {
         }
         bool StartObject() {
             lastKey = "";
+            this->objectDepth++;
             std::cout << "StartObject()" << std::endl; return true;
-            objectDepth++;
         }
         bool Key(const char* str, rapidjson::SizeType length, bool copy) {
             lastKey = str;
+
+            if (strcmp(str,"mapId") == 0) {
+                cout << "";
+            } else if (strcmp(str,"idProperty") == 0) {
+                cout << "";
+            } else if (strcmp(str, "properties") == 0) {
+
+            } else if (strcmp(str,"name") == 0) {
+
+            } else if (strcmp(str,"collections") == 0) {
+                insideCollectionsObj = true;
+            } else if (objectDepth == 1) { // We are in at the schema definition level.
+                currentObjectKey = str;
+                JsonSchema jsonSchema = JsonSchema();
+                jsonSchemaSetPtr.insert(make_pair(str, jsonSchema));
+            } else {
+                cout << "We do not have a mapping for the provided key " << str << endl;
+            }
 
             std::cout << "Key(" << str << ", " << length << ", " << std::boolalpha << copy << ")" << std::endl;
             return true;
         }
         bool EndObject(rapidjson::SizeType memberCount) {
+            this->objectDepth--;
             std::cout << "EndObject(" << memberCount << ")" << std::endl; return true;
-            objectDepth--;
         }
-        bool StartArray() { std::cout << "StartArray()" << std::endl; return true; }
-        bool EndArray(rapidjson::SizeType elementCount) { std::cout << "EndArray(" << elementCount << ")" << std::endl; return true; }
+        bool StartArray() {
+            this->arrayDepth++;
+            std::cout << "StartArray()" << std::endl; return true;
+        }
+        bool EndArray(rapidjson::SizeType elementCount) {
+            this->arrayDepth--;
+            std::cout << "EndArray(" << elementCount << ")" << std::endl; return true;
+        }
     };
 
 }
